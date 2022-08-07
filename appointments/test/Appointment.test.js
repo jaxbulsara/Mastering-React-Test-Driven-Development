8f0eaf1fb7031e1bd286bd23affa6c14c7e4b734
Jay Bulsara <jaybulsara@proton.me>
2022-08-07 11:41:59 -0400

    Select first appointment by default.

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -37,8 +37,14 @@ describe('AppointmentsDayView', () => {
 
   const today = new Date();
   const appointments = [
-    {startsAt: today.setHours(12, 0)},
-    {startsAt: today.setHours(13, 0)},
+    {
+      startsAt: today.setHours(12, 0),
+      customer: {firstName: 'Ashley'},
+    },
+    {
+      startsAt: today.setHours(13, 0),
+      customer: {firstName: 'Jordan'},
+    },
   ];
 
   beforeEach(() => {
@@ -79,5 +85,11 @@ describe('AppointmentsDayView', () => {
     expect(container.textContent).toMatch(
       'There are no appointments scheduled for today.'
     );
+  });
+
+  it('selects the first appointment by default.', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.textContent).toMatch('Ashley');
   });
 });

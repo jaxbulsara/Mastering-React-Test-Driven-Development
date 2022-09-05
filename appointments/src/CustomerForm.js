1b05abf69f37b69cf1122bfb2b8ccd6d2eec6a12
Jay Bulsara <jaybulsara@proton.me>
2022-09-05 14:56:15 -0400

    Fixed readOnly error message.

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const CustomerForm = ({firstName}) => {
   return (
     <form id="customer" action="">
-      <input type="text" name="firstName" value={firstName} />
+      <input type="text" name="firstName" value={firstName} readOnly />
     </form>
   );
 };

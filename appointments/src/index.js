dd656a32610b5b38f0eeb6bae3b5893aaa47e757
Jay Bulsara <jaybulsara@proton.me>
2022-09-06 10:56:10 -0400

    Changed to render customer form for manual testing.

--- appointments/src/index.js
+++ appointments/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {AppointmentsDayView} from './AppointmentsDayView';
+import {CustomerForm} from './CustomerForm';
 import {sampleAppointments} from './sampleData';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const component = <AppointmentsDayView appointments={sampleAppointments} />;
+// const component = <AppointmentsDayView appointments={sampleAppointments} />;
+const component = <CustomerForm />;
 root.render(component);
